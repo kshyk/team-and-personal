2048faad724701894867f1f770502da08961616a
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2024-12-05 00:20:35 +0100

    prepare first pipeline

---
+++ playwright.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['list'],
+    [
+      'html',
+      {
+        open: 'never',
+        outputFolder: 'test-results'
+      }
+    ],
+    [
+      'junit',
+      {
+        outputFile: 'test-results/junit-results.xml'
+      }
+    ]
+  ],
+  use: {
+    baseURL: 'https://teamandpersonal.pl',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
+  },
+  projects: [
+    {
+      name: 'setup',
+      testMatch: /global\.setup\.ts/
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+      dependencies: ['setup']
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+      dependencies: ['setup']
+    },
+    {
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      dependencies: ['setup']
+    },
+    {
+      name: 'msedge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      dependencies: ['setup']
+    },
+  ],
+});
