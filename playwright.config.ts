c02ed3f7e18e7764ca1186de8bd3da20b5c15b55
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2024-12-05 03:09:19 +0100

    add videos and screen to zip

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,24 +7,13 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['blob'],
     ['list'],
-    [
-      'html',
-      {
-        open: 'never',
-        outputFolder: 'test-results'
-      }
-    ],
-    [
-      'junit',
-      {
-        outputFile: 'test-results/junit-results.xml'
-      }
-    ]
+    ['html', { open: 'never', outputFolder: 'test-results' }],
+    ['junit', { outputFile: 'test-results/junit/results.xml' }],
   ],
   use: {
     baseURL: 'https://teamandpersonal.pl',
