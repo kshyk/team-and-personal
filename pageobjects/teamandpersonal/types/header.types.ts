bf58f822e31bef6839d66c41a12c92896b5e39cd
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2024-12-09 21:55:21 +0100

    all menu item types

---
+++ pageobjects/teamandpersonal/types/header.types.ts
@@ -0,0 +1,117 @@
+export type SocialMedia = 'facebook' | 'youtube' | 'linkedin' | 'instagram' | 'pinterest' | 'twitter';
+
+export type MainMenuItem =
+  'Team&Personal'
+  | 'O firmie'
+  | 'Wyjazdy Integracyjne'
+  | 'Team building'
+  | 'Szkolenia'
+  | 'Eventy'
+  | 'Kontakt';
+
+export type CommonType = 'Indoor' | 'Outdoor';
+
+export type IntegrationTripType = CommonType | 'Gry miejskie' | 'Gry terenowe';
+export type IntegrationTripIndoorType =
+  'QUEST BOX Pokój zagadek'
+  | 'Impreza integracyjna w Krakowie'
+  | 'Warsztaty Domowe Spa'
+  | 'Warsztaty kulinarne'
+  | 'Kopalnia Wieliczka poza szlakiem…'
+  | 'Warsztaty 3 x R'
+  | 'Grand Prix F1'
+export type IntegrationTripOutdoorType =
+  'Integracyjne Rajdy Rowerowe'
+  | 'Impreza integracyjna na wodzie'
+  | 'Nowa Huta Aktywnie – Czasy PRL-U'
+  | 'Wyjazdy integracyjne Off Road'
+  | 'Zawody strzeleckie'
+  | 'Akademia golfa'
+  | 'Impreza na statku – Rejs po Wiśle'
+  | 'Zwiedzanie Krakowa'
+  | 'Warsztaty fotograficzne'
+  | 'Spływ Kajakowy'
+  | 'Regaty Żeglarskie'
+  | 'Zimowa wyprawa na Ski Turach'
+  | 'Zimowa wyprawa integracyjna'
+  | 'Zimowa integracja Crazy Snow'
+  | 'Olimpic Games'
+  | 'Doskonalenie techniki jazdy'
+export type CityGameType =
+  'Gra Miejska Krakowskie Zakamarki'
+  | 'Gra Miejska Legendy Krakowa'
+  | 'Gra miejska 3 Kultury Krakowa'
+export type FieldGameType =
+  'Gra terenowa Quest'
+  | 'Gra Terenowa Top Secret'
+  | 'Gra Terenowa Kod Dostępu'
+  | 'Gra terenowa Władca Pierścieni'
+
+export type TeamBuildingType = CommonType | 'Artystyczne' | 'Projekty CSR';
+export type TeamBuildingIndoorType =
+  'Gra symulacyjna Trident'
+  | 'Sztafeta komunikacji'
+  | 'Rurociąg przyjaźni'
+  | 'Wspólny dom'
+  | 'Team Puzzle'
+export type TeamBuildingOutdoorType =
+  'Team building Games'
+  | 'Chodzenie po ogniu'
+  | 'Smocze Łodzie'
+  | 'Gra szkoleniowa Inwestycja'
+export type ArtisticType =
+  'Masaż Dźwiękiem – Sesja Antystresowa'
+  | 'Warsztaty Świat Tańca'
+  | 'Warsztaty wokalne'
+  | 'Warsztaty teatralne'
+  | 'Warsztaty Bębnowe'
+  | 'Warsztaty Filmowe'
+export type CSRType = 'Projekty CSR' | 'Projekty Eko'
+
+export type TrainingType =
+  'Szkolenia integracyjne'
+  | 'Szkolenia rozwojowe'
+  | 'Coaching'
+  | 'Team coaching';
+export type IntegrativeTrainingType = 'Szkolenia integracyjne'
+export type DevelopmentTrainingType =
+  'Wypalenie Zawodowe'
+  | 'Asertywność'
+  | 'Budowanie zespołu'
+  | 'Efektywna komunikacja'
+  | 'Kreatywność w działaniu'
+  | 'Prezentacja'
+  | 'Przywództwo'
+  | 'Sztuka motywowania'
+  | 'Profesjonalny dobór pracowników'
+  | 'Warsztat World Cafe'
+  | 'Zarządzanie czasem'
+  | 'Zarządzanie zmianą'
+export type CoachingType = 'Coaching' | 'Coaching dla managerów'
+export type TeamCoachingType = 'Team Coaching'
+
+export type EventType = CommonType | 'Pikniki' | 'Konferencje';
+export type EventIndoorType =
+  'Karnawał Wenecki'
+  | 'Marvel Heroes Event'
+  | 'Casyno'
+  | 'Pokazy Tańca'
+  | 'Pokazy Barmańskie'
+  | 'Karnawał w Rio'
+  | 'Wieczór Kubański'
+  | 'Wieczór Afrykański'
+  | 'Wieczór Bollywood'
+  | 'Country & Western'
+  | 'Ceperski Wieczór'
+  | 'Mamma Mia Night'
+  | 'Szalone lata Big Bitu'
+  | 'Noc Wikingów'
+  | 'Wieczór Bałkański'
+  | 'Degustacje'
+  | 'Noc Magii'
+  | 'Pokaz Mody'
+export type EventOutdoorType =
+  'Event historyczny – Dawno temu w Krakowie'
+  | 'Pokazy Ognia'
+export type PicnicType = 'Piknik Firmowy'
+export type ConferenceType = 'Konferencje Organizacja'
