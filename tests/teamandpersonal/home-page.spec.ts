adb961ecbff4629a7189baef2f1515d516dda7bc
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2024-12-15 22:59:48 +0100

    click on few main navi options

---
+++ tests/teamandpersonal/home-page.spec.ts
@@ -0,0 +1,19 @@
+import {expect, Page, test} from "@playwright/test";
+import HomePage from "../../pageobjects/teamandpersonal/home-page";
+import Header from "../../pageobjects/teamandpersonal/header";
+
+test.describe('HomePage', () => {
+  test.describe.configure({mode: 'serial'});
+
+  let page: Page;
+
+  test.beforeAll(async ({browser}) => {
+    page = await browser.newPage();
+    await new HomePage(page).goto();
+    await new Header(page).openTeamAndPersonal();
+  });
+
+  test('Team&Personal option navigates to Home page', async () => {
+    expect(page.url()).toBe('https://teamandpersonal.pl/');
+  });
+});
